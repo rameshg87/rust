d556cdee70d2fb010bf5150a3bfa61a3ee7c2502
Ramakrishnan G <rameshg87@gmail.com>
2022-03-27 21:41:58 +0530

    Update repo

---
+++ rbe/src/formatting_color.rs
@@ -0,0 +1,43 @@
+use std::fmt;
+
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "RGB ({red}, {green}, {blue}) 0x{red:02X}{green:02X}{blue:02X}",
+            red = self.red,
+            green = self.green,
+            blue = self.blue
+        )
+    }
+}
+
+fn main() {
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *color);
+    }
+}
---
+++ rbe/src/primitives.rs
@@ -0,0 +1,15 @@
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
+fn main() {
+    let mut mutable = 12;
+    println!("mutable {}", mutable);
+    mutable = 21;
+    mutable = 4294967296i64;
+    println!("mutable {}", mutable);
+    print_type_of(&mutable);
+    let mutable = true;
+    println!("mutable {}", mutable);
+    print_type_of(&mutable);
+}
